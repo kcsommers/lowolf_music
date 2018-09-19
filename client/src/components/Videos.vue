dd3bdfa5ebcd407fa53eeab1ee1f5194ca3986d2
Mark Sommers <kacysommers@gmail.com>
2018-09-19 16:33:11 -0700

    added slider functionality;

--- client/src/components/Videos.vue
+++ client/src/components/Videos.vue
@@ -1,37 +1,15 @@
 <template>
-  <div class="video-wrapper">
-    <img src="@/assets/images/laura_laura.jpg" alt="Lo Wolf and Laura Blackley" />
-    <div class="play-btn-wrapper"><i class="fas fa-play"></i></div>
+  <div class="videos-wrapper">
+    <VideoSlider />
   </div>
 </template>
 
 <script>
+import VideoSlider from '@/components/VideoSlider'
 export default {
-  name: 'Video'
+  name: 'Video',
+  components: {VideoSlider}
 }
 </script>
-
-<style lang="scss">
-  .video-wrapper {
-    position: relative;
-
-    img {
-      width: 100%;
-    }
-
-    .play-btn-wrapper {
-      position: absolute;
-      top: 50%;
-      left: 0;
-      width: 100%;
-      text-align: center;
-      i {
-        color: #f2f2f2;
-        font-size: 4em;
-        opacity: 0.8;
-      }
-    }
-  }
-</style>
 
 
